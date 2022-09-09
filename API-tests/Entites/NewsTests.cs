0ce236380b995ac5b97f9cf5fc984d0070b548c7
alexrocha97 <venturarocha08@gmail.com>
2022-09-09 12:22:01 -0300

    Inicio dos teste de unidade

---
+++ API-tests/Entites/NewsTests.cs
@@ -0,0 +1,24 @@
+﻿using API.Domain;
+using API.Entities;
+using API.Entities.Enums;
+using Xunit;
+
+namespace API_tests.Entites
+{
+    public class NewsTests
+    {
+        [Fact]
+        public void News_Validate_Title_Length()
+        {
+            var result = Assert.Throws<DomainException>(() => new News(
+            "",
+            "",
+            "",
+            "",
+            "",
+            status: Status.Active));
+
+            Assert.Equal("O título deve ter até 90 caracteres", result.Message);
+        }
+    }
+}
